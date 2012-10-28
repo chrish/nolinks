f54aa17121e0eb44f056edb9312e47ff71e7788a
bjelleklang <chris@bjelleklang.org>
2012-10-28 00:17:35 +0000

    Removing old stuff, separated python scripts and web

--- parse/parsedump.py
+++ parse/parsedump.py
@@ -3,21 +3,28 @@
 import MySQLdb
 import sys
 import time 
-import Db
+import os, oursql
 
 start_time = time.time()
 
 data = open("../../data.txt", "r")
-sql = "INSERT INTO nolinks (pageTitle) VALUES (%s)"
-cursor = Db.connect()
+sql = "INSERT INTO nolinks (pagetitle, pagelen) VALUES (?, ?)"
+
+db = oursql.connect(db='u_bjelleklang',
+        host="sql",
+        read_default_file=os.path.expanduser("~/.my.cnf"),
+        charset=None,
+        use_unicode=False
+)
+
+cursor = db.cursor()
 i = 0
 
 
-
 for line in data:
-	words = line.split('\t')
-	cursor.execute(sql, words[0])
-	cursor.execute("commit")
+    words = line.split('\t')
+    cursor.execute(sql, (words[0], words[3]))
+    cursor.execute("commit")
 
 cursor.close ()
 conn.close ()
