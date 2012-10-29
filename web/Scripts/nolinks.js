740cbd77d23b08561af8a0ee5c00e28dfc4c2790
bjelleklang <chris@bjelleklang.org>
2012-10-29 07:14:57 +0000

    For some reason a number of changes didn't make it in the last commit, so adding now

--- web/Scripts/nolinks.js
+++ web/Scripts/nolinks.js
@@ -1,14 +1,104 @@
+var page;
+var numPages;
+
 $(document).ready(function(){
 	
 	$('a.toggle').click(function(){
-		$(this).parent('div').find('p').toggle();
 		
 		if($(this).text() == "Hide"){
 			$(this).html("Show");
+            $(this).parent('div').animate({
+                height: 20,
+            });
 		} else {
 			$(this).html("Hide");
+            $(this).parent('div').animate({   
+                height: 120,
+            }); 
 		}
 	});
 	
-	$('#datatable').dataTables();
+    getNumberOfArticles(1);
+    page = 0;
+
+    $.get("api.php?mode=list", function(data){
+        printTable(data);
+    }, "json");
+
+    $('select.pagelist').change(function(){
+        var pval = $(this).val();
+        $('select.pagelist').val(pval);
+
+        page = pval;
+        getPage();
+    });
+
+    $('a.prev').click(function(){
+        if (page > 0){
+            page = page-1;
+        }
+
+        getPage();
+    });
+
+    $('a.next').click(function(){   
+        if (page < numPages){ 
+            page = page+1;
+        }
+
+        getPage();
+    });  
+
+    $('body').delegate('a.verify', 'click', function(){
+        var id = $(this).attr('id');
+        $.get('api.php?mode=remove&p=' + id, function(data){
+            $('#' + id).closest('tr').remove();
+        }, "json");
+    });
 });
+
+function printTable(data){
+    var tbl = $('#datatable tbody');
+    var cl = "odd";
+
+    for(var i=0; i<data.length; i++){
+        var s = '<tr class="' + cl + '"><td><a href="http://en.wikipedia.org/wiki/' + data[i][1] + '" target="_blank">' + data[i][1] + '</td>'
+                + '<td>' + data[i][2] + '</td><td>' + data[i][3] + '</td><td><a href="#" id="' + data[i][0] + '" class="verify">Remove from list</a></td></tr>';
+
+        tbl.append(s);
+        
+        if (cl=="odd") {
+            cl="even";
+        }else {
+            cl="odd";
+        }
+    }
+
+}
+
+function resetTable(){
+    $('#datatable tbody').empty();
+}
+
+function getPage(){
+    $.get("api.php?mode=list&p=" + page, function(data){
+        resetTable();
+        printTable(data);
+    }, "json"); 
+}
+
+function getNumberOfArticles(curPage){
+    $.get("api.php?mode=count", function(data){
+        var num = data.NUM;
+
+        var np = Math.ceil(num/1000);
+        var pl = $('select.pagelist');
+        
+        for(var i=0; i<np; i++){
+            var display = i+1;
+            pl.append('<option value="' + i + '">Page ' + display + '</option>');
+        }
+
+        numPages = np;
+    }, "json");
+}
--- web/api.php
+++ web/api.php
@@ -3,27 +3,76 @@
 	 *  
 	 *  
 	 * */
-	$FILE = "../../data.txt";
-	 
+
+	require "/home/bjelleklang/public_html/db.php";
+
 	if (isset($_GET['mode'])){
 		$mode = $_GET['mode'];
-		
-		print(json_encode(getListSegment(0, 1000)));
-		
+		$data = "";
+        $p = 0;
+
+		if ($mode == "list"){
+            if (isset($_GET['p'])){
+                $p = $_GET['p'];
+            }
+			$data = getListSegment($p*1000);
+		} else if ($mode == "count"){
+			$data = getNumArticles();
+		} else if ($mode == "remove" && isset($_GET['p'])){
+            removeArticle($_GET['p']);
+        }
+
+		print json_encode($data);
 	}
+
 	
-	function getListSegment($startAt=0, $number=1000){
-		$text = explode("\n", file_get_contents("../../data.txt"));	
-		$return = array();
-		
-		if ($startAt < $text){
-			$i = $startAt;	
-			while ($i < length($text) && $i < $number){
-				$t = explode("\t", $text);
-				$return[] = $t[0];
-			}
-		}
-		
+	function getListSegment($p){
+		$db = getDbConn();
+		$q = "SELECT pageid, pagetitle, pagelen, numlinks FROM nolinks WHERE checked = 0 LIMIT 1000" 
+			. " OFFSET " . mysql_real_escape_string($p) . ";";
+
+        $return = array();
+
+        $tmp = mysql_query($q);
+        while ($data = mysql_fetch_row($tmp)){
+            $return[] = $data;
+        }
+
 		return $return;
 	}
+
+    function getNumArticles(){
+        $db = getDbConn();
+
+        $q = "SELECT COUNT(pageid) as NUM FROM nolinks WHERE checked = 0";
+    	$tmp = mysql_fetch_assoc(mysql_query($q));
+	    return $tmp;
+    }
+
+    function removeArticle($p){
+//        if (!isBlocked($_SERVER['REMOTE_ADDR'])){
+            $db = getDbConn();
+            $q = "UPDATE nolinks SET checked = 1 WHERE pageid = " . mysql_real_escape_string($p);
+            mysql_query($q);
+
+            vRegister($_SERVER['REMOTE_ADDR'], $p);
+ //       }
+    }
+
+    function isBlocked($ip){
+        $db = getDbConn();
+        $q = "SELECT ip FROM blocklist WHERE bip = '" . mysql_real_escape_string($ip) . "'";
+        $num = mysql_num_rows(mysql_query($q, $db));
+        
+        if ($num ==0) 
+            return false; 
+        else 
+            return true;
+    }
+
+    function vRegister($ip, $pid){
+        $db = getDbConn();
+        $q = "INSERT INTO verification VALUES (null, '" . mysql_real_escape_string($ip) . "', '" . mysql_real_escape_string($pid) . "', null)";
+        mysql_query($q);
+    }
 ?>
